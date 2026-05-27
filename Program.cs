9980dd7b1633ec6973197276d1a79a016a1274c4
BernardoSoares303 <bbnado34@gmail.com>
2026-05-27 19:29:44 -0300

    procedimento de preencher vetor

--- Program.cs
+++ Program.cs
@@ -2,6 +2,18 @@
 {
     public static void Main()
     {
-        
+        int[] vetor = new int[12];
+
+    }
+
+    public void preenchervetor(int[] vetor)
+    {
+        Random r = new Random();
+
+        for(int i = 0; i < vetor.Length; i++)
+        {
+            vetor[i] = r.Next(5,50);
+        }
     }
 }
+
